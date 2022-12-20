ca6a3c8ab770ce366d169fce68b631be8f7fb7ec
Pavel <lp.qroovemaster@gmail.com>
2022-12-21 00:07:21 +0300

    Final classes in the end of lesson

--- src/LessonCode2.java
+++ src/LessonCode2.java
@@ -1,5 +1,16 @@
 public class LessonCode2 {
     public static void main(String[] args) {
-        System.out.println("Hello World 2");
+
+        int a = 10;
+        int b = 4;
+
+        int c = 5;
+
+        double result = a / (double) b;
+        double result2 = b * (double) c;
+
+        System.out.println(result);
+        System.out.println(result2);
+
     }
 }
